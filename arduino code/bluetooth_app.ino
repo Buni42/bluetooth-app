e1f337da091356c988ecdb24e651e8c3293c9419
Bunyamin <55626362+Buni42@users.noreply.github.com>
2023-02-24 05:05:16 +0100

    Update bluetooth_app.ino

--- arduino code/bluetooth_app.ino
+++ arduino code/bluetooth_app.ino
@@ -12,8 +12,6 @@ void setup() {
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
   pinMode(relay4, OUTPUT);
-  Serial.begin(9600);
-
 }
 
 void loop() {
@@ -21,38 +19,31 @@ void loop() {
   if(HC06.available() > 0) //When HC06 receive something
   {
     char receive = HC06.read(); //Read from Serial Communication
-    if(receive == '1') //If received data is I, turn on the relay 
+    if(receive == '1') //If received data is 1, turn on the relay 
     {
-      Serial.print('1');
       digitalWrite(relay1, HIGH); 
-      
     }
-    if(receive == '0') //If received data is I, turn on the relay 
+    if(receive == '0') //If received data is 0, turn off the relay 
     {
-      Serial.print('0');
       digitalWrite(relay1, LOW); 
-      
     }
     if(receive == '2') //If received data is 2, turn on the relay2
     {
-      Serial.print("2");
       digitalWrite(relay2, HIGH); 
-      delay(1000);
-      digitalWrite(relay2, LOW);
     }
     if(receive == '3') //If received data is 3 turn on the relay3
     {
-      Serial.print("3");
       digitalWrite(relay3, HIGH); 
-      delay(1000);
-      digitalWrite(relay3, LOW);
     }
     if(receive == '4') //If received data is 4, turn on the relay4 
     {
-      Serial.print("4");
       digitalWrite(relay4, HIGH); 
-      delay(1000);
+    }
+    if(receive == '9')//if 9, act like a button and turn off the relay
+    {
       digitalWrite(relay4, LOW);
+      digitalWrite(relay3, LOW);
+      digitalWrite(relay2, LOW);
     }
   }
 
